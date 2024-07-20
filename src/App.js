ee8926ef835dafc1e084db0ea3a6ef7ea5181f43
Shreyaa173 <shreya186btcse23@igdtuw.ac.in>
2024-07-21 03:56:37 +0530

    account

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route path="login" element={<Login/>}/>
           <Route path="signup" element={<Signup/>}/>
           <Route path="/about" element={<About/>}/>
-          <Route path="connect" element={<Connect/>}/>
+          <Route path="connect" element={<About/>}/>
         </Routes>
       </Router>
     </div>

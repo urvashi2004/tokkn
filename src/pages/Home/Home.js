6ad521013e64196c3d75fbbe3da43fe267d24936
urvashi2004 <uy2204@gmail.com>
2024-07-20 19:52:14 +0530

    Home page badly

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,36 +9,24 @@ const Home = () => {
 
                 <div className="card-main">
                     <HoverCard name="Trust and peace of mind" 
-                    details="Craft stunning, responsive websites
-                    from scratchwith HTML, CSS, and JavaScript.
-                    Dive deep into frameworks like React and
-                    backend tech like Node.js!"/>
+                    details="Hello"/>
                 </div>
 
                 <div className="card-main">
-                    <HoverCard name="Adapt your budget"
-                    details="Build sleek mobile apps with Android
-                    or iOS, and master cross-platform tools
-                    like Flutter. Create, test, and optimize
-                    your app for any device!"/>
+                    <HoverCard name="Let us adapt your budget"
+                    details="Hello2"/>
                 </div>
 
                 <div className="card-main">
                     <HoverCard name="A new kind of employee"
-                    details="Step into the future with AR/VR!
-                    Create immersive 3D worlds using Unity
-                    or Unreal Engine, and script interactive
-                    experiences with ease."/>
+                    details="Hello3"/>
                 </div>
 
                 <div className="card-main">
                     <HoverCard name="Rewards users in real time"
-                    details="Step into the future with AR/VR!
-                    Create immersive 3D worlds using Unity
-                    or Unreal Engine, and script interactive
-                    experiences with ease."/>
+                    details="Hello4"/>
                 </div>
-                
+
             </div>
         </div>
     );
